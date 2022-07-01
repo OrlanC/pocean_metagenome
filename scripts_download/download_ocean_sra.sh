018048810afb0b1616a9d7c51d22a66709c57adb
Claudia Zirión Martínez <75807915+Czirion@users.noreply.github.com>
2022-07-01 12:42:57 -0500

    Update download_ocean_sra.sh

--- scripts_download/download_ocean_sra.sh
+++ scripts_download/download_ocean_sra.sh
@@ -1,13 +1,13 @@
 #PBS -N sraToolkit
 #PBS -q default
 #PBS -l nodes=1:ppn=16,mem=40g,vmem=40g,walltime=100:00:00
-#PBS -e /LUSTRE/usuario/czirion/sradownload.error
-#PBS -o /LUSTRE/usuario/czirion/sradownload.output
+#PBS -e /LUSTRE/usuario/username/sradownload.error
+#PBS -o /LUSTRE/usuario/username/sradownload.output
 #PBS -V
 
 
 module load sratoolkit/2.9.6
 
-cd /LUSTRE/usuario/czirion/
+cd /LUSTRE/usuario/username/
 
 cat path_file.txt | while read line; do wget $line; done
