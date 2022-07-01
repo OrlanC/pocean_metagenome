3eb69b285896fbee610311e4e4e3bdf6856b22fb
Claudia Zirión Martínez <75807915+Czirion@users.noreply.github.com>
2022-07-01 12:43:20 -0500

    Update make_paths.sh

--- scripts_download/make_paths.sh
+++ scripts_download/make_paths.sh
@@ -1,13 +1,13 @@
 #PBS -N sraToolkit
 #PBS -q default
 #PBS -l nodes=1:ppn=16,mem=40g,vmem=40g,walltime=100:00:00
-#PBS -e /LUSTRE/usuario/czirion/srapath.error
-#PBS -o /LUSTRE/usuario/czirion/srapath.output
+#PBS -e /LUSTRE/usuario/username/srapath.error
+#PBS -o /LUSTRE/usuario/username/srapath.output
 #PBS -V
 
 
 module load sratoolkit/2.9.6
 
-cd /LUSTRE/usuario/czirion/
+cd /LUSTRE/usuario/username/
 
 cat SRA_Acc_List.txt | while read line; do srapath $line >> path_file.txt; done
